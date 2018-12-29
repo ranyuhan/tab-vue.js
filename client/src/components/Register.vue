04241d718a7d68047d61c506060a374330aec7dd
AveryJin <avery.jin@gmail.com>
2018-12-29 17:21:06 +0800

    setting up the register page

---
+++ client/src/components/Register.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <form>
+      <input type="email" name="email" v-model="email" placeholder="Email" required/>
+      <br>
+      <input type="password" name="password" v-model="password" placeholder="Password" required/>
+      <br>
+      <button type="submit" @click="register">Register</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  name: 'Register',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  // watch: {
+  //   email (value) {
+  //     console.log('email has changed', value)
+  //   }
+  // },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response)
+    }
+  }
+  // mounted () {
+  //   setTimeout(() => {
+  //     this.email = 'hello world'
+  //   }, 2000)
+  // }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- server/src/app.js
+++ server/src/app.js
@@ -14,4 +14,10 @@ app.get('/status', (req, res) => {
   })
 })
 
+app.post('/register', (req, res) => {
+  res.send({
+    message: `Hi there! ${req.body.email} was registered! Horray😀`
+  })
+})
+
 app.listen(process.env.PORT || 8081)
